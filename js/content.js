7e49f3de316581ead90c6259bb562be55637781c
Saeed Naseer <43634420+saeednaseer@users.noreply.github.com>
2026-08-04 13:05:31 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -215,7 +215,6 @@ const siteContent = {
         tags: { ar: "تصميم إعلانات، طباعة", en: "Ad Design, Print" },
         desc: { ar: "تصميم احترافي للوحات الإعلانية والمطبوعات التجارية، يجمع بين الهوية البصرية والرسائل التسويقية لإبراز العلامة التجارية وجذب انتباه العملاء.", en: "Professional advertising and signage design that combines strong visual identity with effective marketing messages to enhance brand presence and capture customer attention." },
         gallery: [
-          "images/Posts/0.webp",
                      "images/Profile/1.webp",
                      "images/Profile/2.webp",
                      "images/Profile/3.webp",
